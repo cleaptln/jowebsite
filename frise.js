d9cf53b26a426f473832cc77e3d60938df6f5a54
Cléa <cleaportolan@icloud.com>
2025-03-11 20:00:05 +0100

    bmx

--- frise.js
+++ frise.js
@@ -1,11 +1,13 @@
 gsap.registerPlugin(ScrollTrigger);
 
 let etapes = gsap.utils.toArray(".etape");
+console.log(etapes);
 
 gsap.to(etapes, {
   xPercent: -100 * (etapes.length - 1),
   ease: "none",
   scrollTrigger: {
+    start: "top top+=80",
     trigger: ".etapes",
     pin: true,
     scrub: 1,
@@ -14,17 +16,32 @@ gsap.to(etapes, {
   }
 });
 
+etapes.forEach((etape, index) => {
+  let cercle = etape.querySelector(".cercleEtape svg");
 
-etapes.forEach((etape) => {
-    const cercle = etape.querySelector(".cercleEtape svg");
-
+  if (cercle) {
     ScrollTrigger.create({
-        trigger: etape,
-        start: "left center", // when the etape is in the center of the viewport
-        end: "right center", // when the etape is in the center of the viewport
-        onEnter: () => gsap.to(cercle, { duration: 0.5, scale: 3, transformOrigin: "center center", y: -100 }),
-        onLeave: () => gsap.to(cercle, { duration: 0.5, scale: 0.2, transformOrigin: "center center", y: 0 }),
-        onEnterBack: () => gsap.to(cercle, { duration: 0.5, scale: 3, transformOrigin: "center center", y: -100 }),
-        onLeaveBack: () => gsap.to(cercle, { duration: 0.5, scale: 0.2, transformOrigin: "center center", y: 0 })
+      trigger: etape,
+      start: "30% right", // quand l'étape est à 40% du bord gauche
+      end: "10% left", // quand l'étape est au centre de la vue
+      onEnter: () => {
+        console.log(`onEnter ${index}`, etape);
+        gsap.to(cercle, { duration: 0.5, scale: 3, transformOrigin: "center center", y: -100 });
+      },
+      onLeave: () => {
+        console.log(`onLeave ${index}`, etape);
+        gsap.to(cercle, { duration: 0.5, scale: 0.2, transformOrigin: "center center", y: 0 });
+      },
+      onEnterBack: () => {
+        console.log(`onEnterBack ${index}`, etape);
+        gsap.to(cercle, { duration: 0.5, scale: 3, transformOrigin: "center center", y: -100 });
+      },
+      onLeaveBack: () => {
+        console.log(`onLeaveBack ${index}`, etape);
+        gsap.to(cercle, { duration: 0.5, scale: 0.2, transformOrigin: "center center", y: 0 });
+      }
     });
+  } else {
+    console.log(`No cercle found in ${index}`, etape);
+  }
 });
--- podium.js
+++ podium.js
@@ -3,7 +3,8 @@ const or = document.querySelector('.podium-or');
 const argent = document.querySelector('.podium-argent');
 const bronze = document.querySelector('.podium-bronze');
 
-gsap.registerPlugin(ScrollTrigger);
+
+gsap.registerPlugin(ScrollTrigger,MotionPathPlugin);
 
 ScrollTrigger.create({
     trigger: podiums,
@@ -13,11 +14,32 @@ ScrollTrigger.create({
         gsap.to(argent, { height: '200px', delay: 0.2, duration: 1, ease: "power2.inOut" });
         gsap.to(bronze, { height: '150px', duration: 1, ease: "power2.inOut" });
     },
-    onLeaveBack: () => {
+    onLeave: () => {
         gsap.to(or, { height: '50px', duration: 0.5, ease: "power2.inOut" });
         gsap.to(argent, { height: '50px', duration: 0.5, ease: "power2.inOut" });
         gsap.to(bronze, { height: '50px', duration: 0.5, ease: "power2.inOut" });
-    }
+    },  
 });
 
+
+
+gsap.to(".bmx", {
+    motionPath: {
+        path: [
+            { x: 0, y: 0 },   // Départ
+            { x: window.innerWidth / 2, y: -150 }, // Milieu de l'écran, monte un peu
+            { x: window.innerWidth, y: 0 }  // Arrivée, redescend
+        ],
+        curviness: 1.5, // Adoucit la courbe
+        autoRotate: true // Oriente le BMX dans la direction
+    },
+    duration: 2, // Temps total de l’animation
+    ease: "power1.inOut",
+    scrollTrigger: {
+        trigger: "body",
+        start: "top top",
+        end: "bottom top",
+        scrub: 1, // Animation fluide avec le scroll
+    }
+});
 
