f418c31924f46d6b1bb51455573374d5073e5e65
Cléa <cleaportolan@icloud.com>
2025-03-17 19:37:31 +0100

    leon section

--- leon.js
+++ leon.js
@@ -1,51 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
-    gsap.registerPlugin(MotionPathPlugin);
+gsap.registerPlugin(ScrollTrigger);
 
-    const medals = document.querySelectorAll('.med');
-    const radius = 150; // Rayon de l'arc de cercle
+let items = gsap.utils.toArray(".rotator li");
 
-    medals.forEach((medal, index) => {
-        const delay = index * 0.5; // Délai pour chaque médaille
-
-        gsap.timeline({ repeat: -1, yoyo:true })
-            .to(medal, {
-                duration: 1,
-                delay: delay,
-                motionPath: {
-                    path: [
-                        { x: radius * Math.cos(270 * (Math.PI / 180)), y: radius * Math.sin(270 * (Math.PI / 180)) },
-                        { x: radius * Math.cos(225 * (Math.PI / 180)), y: radius * Math.sin(225 * (Math.PI / 180)) },
-                        { x: radius * Math.cos(180 * (Math.PI / 180)), y: radius * Math.sin(180 * (Math.PI / 180)) },
-                        { x: radius * Math.cos(135 * (Math.PI / 180)), y: radius * Math.sin(135 * (Math.PI / 180)) },
-                        { x: radius * Math.cos(90 * (Math.PI / 180)), y: radius * Math.sin(90 * (Math.PI / 180)) }
-                    ],
-                    curviness: 1.5,
-                    autoRotate: true
-                },
-                ease: "power1.inOut"
-            })
-            .to(medal, {
-                duration: 0.5,
-                motionPath: {
-                    path: [{ x: radius * Math.cos(90 * (Math.PI / 180)), y: radius * Math.sin(90 * (Math.PI / 180)) }],
-                    curviness: 1.5,
-                    autoRotate: true
-                },
-                ease: "power1.inOut"
-            })
-            .to(medal, {
-                duration: 1,
-                motionPath: {
-                    path: [
-                        { x: radius * Math.cos(135 * (Math.PI / 180)), y: radius * Math.sin(135 * (Math.PI / 180)) },
-                        { x: radius * Math.cos(180 * (Math.PI / 180)), y: radius * Math.sin(180 * (Math.PI / 180)) },
-                        { x: radius * Math.cos(225 * (Math.PI / 180)), y: radius * Math.sin(225 * (Math.PI / 180)) },
-                        { x: radius * Math.cos(270 * (Math.PI / 180)), y: radius * Math.sin(270 * (Math.PI / 180)) }
-                    ],
-                    curviness: 1.5,
-                    autoRotate: true
-                },
-                ease: "power1.inOut"
-            });
-    });
+gsap.to(items, {
+    opacity: 1,
+    y: 0,
+    stagger: 2, // Temps entre chaque apparition
+    duration: 0.8,
+    scrollTrigger: {
+        trigger: ".disciplines",
+        start: "top center",
+        toggleActions: "play none none none"
+    }
 });
